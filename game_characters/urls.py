8f24c7a13164d498a7436fce57e478e6da666b83
Tyler Worth <tylerworth.media@gmail.com>
2025-10-12 12:08:44 +0100

    Fix missing character_detail view

--- game_characters/urls.py
+++ game_characters/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     # Character pages (expand as needed)
     path('list/', views.characters_view, name='character_list'),
-    # path('create/', views.character_create, name='character_create'),  # Add a new character
-    path('<int:pk>/', views.character_detail, name='character_detail'),# View one character
-    path('<int:pk>/edit/', views.character_edit, name='character_edit'), # Edit a character
-    path('<int:pk>/delete/', views.character_delete, name='character_delete'), # Delete a character
 ]
