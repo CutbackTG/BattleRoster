dd025c628930370807525bce284d9686515e2254
Tyler Worth <tylerworth.media@gmail.com>
2025-10-12 12:24:34 +0100

    Fix URLs and static/media config

--- battleroster_project/urls.py
+++ battleroster_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from game_characters import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,9 +11,7 @@ urlpatterns = [
     path('characters/', include('game_characters.urls')),
 ]
 
-# Static files for development
-from django.conf import settings
-from django.conf.urls.static import static
-
+# ✅ Serve static & media files only in DEBUG mode (local dev)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
