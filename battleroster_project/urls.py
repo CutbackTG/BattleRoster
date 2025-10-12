c2769dc5ce32182164c9f5c4c61efb26a85db5d7
Tyler Worth <tylerworth.media@gmail.com>
2025-10-12 13:23:29 +0100

    Temporarily disable accounts urls to debug recursion

--- battleroster_project/urls.py
+++ battleroster_project/urls.py
@@ -5,6 +5,6 @@ from game_characters import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
-    path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('accounts.urls')),
     path('characters/', include('game_characters.urls')),
 ]
