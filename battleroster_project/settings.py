e31bdc65ae704643dc89dd2a520403a8104a1fbb
Tyler Worth <tylerworth.media@gmail.com>
2025-10-12 12:56:04 +0100

    Fix KeyError en-us by enabling USE_I18N

--- battleroster_project/settings.py
+++ battleroster_project/settings.py
@@ -136,8 +136,9 @@ GOOGLE_SHEETS_ID = os.getenv('GOOGLE_SHEETS_ID', '')
 GOOGLE_SHEETS_RANGE = os.getenv('GOOGLE_SHEETS_RANGE', 'Characters!A2:Z')
 
 LANGUAGE_CODE = 'en-us'
-USE_I18N = False
-USE_L10N = False
+USE_I18N = True
+USE_TZ = True
+USE_L10N = True
 
 # --- Logging ---
 LOGGING = {
