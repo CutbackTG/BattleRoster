cc78035a0ba226d9f9cac26ec15a45cb6b8fbe0d
Tyler Worth <tylerworth.media@gmail.com>
2025-10-14 00:31:24 +0100

    Remove add_character route causing import error

--- game_characters/urls.py
+++ game_characters/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.characters_view, name='characters'),
-    path('add/', views.add_character, name='add_character'),
-    path('edit/<int:character_id>/', views.edit_character, name='edit_character'),
-    path('delete/<int:character_id>/', views.delete_character, name='delete_character'),
-
     path('party/', views.party_view, name='party'),
     path('contact/', views.contact_view, name='contact'),
 ]
