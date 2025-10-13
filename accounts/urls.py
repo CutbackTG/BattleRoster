26b903e83fe39f3f62bae437d769dc3c169c3562
Tyler Worth <tylerworth.media@gmail.com>
2025-10-13 22:29:57 +0100

    support signup-login url name

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     # Combined signup + login
     path('signup_login/', views.signup_login_view, name='signup_login'),
+    # Alias with dash to support templates using 'signup-login'
+    path('signup_login/', views.signup_login_view, name='signup-login'),
+
 
     # Logout route
     path('logout/', views.logout_view, name='logout'),
