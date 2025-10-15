e94ef51b110184315e8fe762fb910c2fc7b9fe8a
Tyler Worth <tylerworth.media@gmail.com>
2025-10-15 20:32:08 +0100

    auth.user error found in test file - amended

--- accounts/tests/test_navbar.py
+++ accounts/tests/test_navbar.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-
 class NavbarAuthTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
