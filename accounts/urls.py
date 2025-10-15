f6cb1171aecc9196b006c8ad45bbb48b7efc5f63
Tyler Worth <tylerworth.media@gmail.com>
2025-10-15 22:05:45 +0100

    urls.py and views.py updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from game_characters import views as game_views  # import your views
 
 urlpatterns = [
-    path("", views.signup_login_view, name="signup_login"),
-    path("logout/", views.logout_view, name="logout"),
+    path('admin/', admin.site.urls),
+    path('', game_views.index_view, name='home'),  # homepage
+    path('accounts/', include('accounts.urls')),
+    path('characters/', game_views.characters_view, name='characters'),
+    path('party/', game_views.party_view, name='party'), 
 ]
