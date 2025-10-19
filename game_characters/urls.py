f9f1b63e107c66f3af2cd57388adb42aae8208bb
Tyler Worth <tylerworth.media@gmail.com>
2025-10-19 23:24:16 +0100

    Update character views and urls, merge edit into characters_view, remove index_view from app

--- game_characters/urls.py
+++ game_characters/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'game_characters'
+
 urlpatterns = [
     # List and create characters
     path('', views.characters_view, name='characters'),
 
-    # Edit a character
+    # Edit an existing character (characters_view handles editing when pk is provided)
     path('edit/<int:pk>/', views.characters_view, name='character_update'),
 
     # Delete a character
